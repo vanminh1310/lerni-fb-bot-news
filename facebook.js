07bfe5011590e96ac296b0ba2f10189640c2ab67
vanminh1310 <53778428+vanminh1310@users.noreply.github.com>
2020-04-28 11:33:17 +0700

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -46,10 +46,7 @@ function Facebook(accessToken, groupId)
         let date = moment().format('dddd').toUpperCase() + ' NGÀY '+ moment().format('LL').toUpperCase();
         formPublish.append('message',
             `# TIN TỨC BUỔI TRƯA ${date}\n` +
-            '\n' +
-            '## thông tin\n' +
-            '* Bài viết được tự động đăng lúc 12 giờ trưa hàng ngày\n' +
-            '* Đây là post tự động được tạo bởi Lerni và lấy dữ liệu từ VnExpress qua RSS các bạn có thể check source code [tại đây](https://bit.ly/lerni-bot-fb)');
+            
         formPublish.append('formatting', 'MARKDOWN');
 
         for (let i = 0; i < images.length ; i++) {
